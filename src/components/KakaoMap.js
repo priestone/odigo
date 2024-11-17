3541f6d2624f731c954c2acb9683c530516a0938
preistone <aion97@naver.com>
2024-11-17 23:30:01 +0900

    marker setting

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,9 +1,46 @@
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import styled from "styled-components";
 import UseGeolocation from "./UseGeolocation";
+import { locationData } from "../api";
+import { useEffect, useState } from "react";
+
+const parseLatLng = (coordinateString) => {
+  const [latStr, lngStr] = coordinateString.split(", ");
+  const lat = parseFloat(latStr.replace("N", ""));
+  const lng = parseFloat(lngStr.replace("E", ""));
+  return { lat, lng };
+};
 
 const KakaoMap = () => {
+  const [placeData, setPlaceData] = useState("");
   const location = UseGeolocation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          response: {
+            body: {
+              items: { item },
+            },
+          },
+        } = await locationData("카페");
+
+        console.log("API Items:", item);
+
+        const parsedItems = item.map((item) => {
+          const { lat, lng } = parseLatLng(item.coordinates);
+          return { ...item, lat, lng };
+        });
+
+        setPlaceData(parsedItems);
+        // console.log(item);
+        // console.log(kakaoData);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
   if (!location.loaded) return <p>Loading...</p>;
   if (location.error) return <p>Error: {location.error.message}</p>;
@@ -22,7 +59,16 @@ const KakaoMap = () => {
           zIndex: `-1`,
         }}
         level={3}
-      ></Map>
+      >
+        {placeData &&
+          placeData.map((place, index) => (
+            <MapMarker
+              key={`${place.tel}`}
+              position={{ lat: place.lat, lng: place.lng }} // 마커를 표시할 위치
+              title={place.placeName} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            />
+          ))}
+      </Map>
     </>
   );
 };
