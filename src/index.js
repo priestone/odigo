a592bda44540d853d958cbe4f4cf7b32215f2be2
preistone <aion97@naver.com>
2024-11-22 16:29:35 +0900

    https

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
 import Router from "./Router";
 import { GlobalStyled } from "./GlobalStyled";
 
@@ -8,7 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyled></GlobalStyled>
-    {/* <App /> */}
     <Router></Router>
   </React.StrictMode>
 );
