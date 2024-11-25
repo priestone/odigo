2c28b3a74363665b27d3c0614017e8a8b02996d5
사재원 <aion97@naver.com>
2024-11-25 23:37:31 +0900

    api cors...

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 const APIKEY = "3d9ed2aa-7786-4f16-89a6-90619bf6ce60";
 
-const baseURL = `https://cors-anywhere.herokuapp.com/https://api.kcisa.kr/openapi/API_TOU_048/request?serviceKey=${APIKEY}&numOfRows=50&pageNo=1&keyword=`;
+const baseURL = `https://9vt921i7q6.execute-api.eu-north-1.amazonaws.com/development?serviceKey=${APIKEY}&numOfRows=50&pageNo=1&keyword=`;
 
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
+    // "Access-Control-Allow-Origin": "*",
   },
 };
 
